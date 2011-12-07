b46a0d3ac741d839249fd3930d9e5cc6327c8958
jdejong <jdejong@sparknettech.com>
2011-12-07 11:48:56 -0500

    added missing item

--- lib/generators/devise_traceable/templates/migration.rb
+++ lib/generators/devise_traceable/templates/migration.rb
@@ -3,6 +3,7 @@ class DeviseCreate<%= table_name.camelize.singularize %>Tracings < ActiveRecord:
     create_table :<%= table_name.singularize %>_tracings do |t|
       t.integer  :<%= table_name.classify.foreign_key  %>
       t.datetime :sign_in_at
+      t.datetime :sign_out_at
       t.string :ip
       t.text :user_agent 
     end
